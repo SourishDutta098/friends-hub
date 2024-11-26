41c8e633018b0fd764975aef33c3543580289d7d
Sourish Dutta <sourishdutta098@gmail.com>
2024-11-27 03:56:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-# TODO: UPDATE THIS FILE FOR DEPLOYMENT
+
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -6,8 +6,6 @@ import os
 
 app = Flask(__name__)
 
-# We can comment this CORS config for the production because we are running the frontend and backend on the same server
-# CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,7 +23,7 @@ def index(filename):
     filename = "index.html"
   return send_from_directory(dist_folder,filename)
 
-# api routes
+
 import routes
 
 with app.app_context():
